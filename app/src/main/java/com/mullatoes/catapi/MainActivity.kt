4ac00b5eafb56ea6b7f2f06dfce7c2f6dc893082
kyeiiih <kevin.kyei@systechafrica.com>
2023-08-14 21:42:20 +0300

    Illustrated working of snackbar

--- app/src/main/java/com/mullatoes/catapi/MainActivity.kt
+++ app/src/main/java/com/mullatoes/catapi/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.mullatoes.catapi
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mullatoes.catapi.ui.theme.CatApiTheme
 import com.mullatoes.catapi.utils.ApiKeyUtil
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +37,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Api Key")
+                    CenterColumn()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CenterColumn() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LaunchSnackBar()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun LaunchSnackBar() {
+
+    val snackBarHostState = remember {
+        SnackbarHostState()
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        }
+    ) {
+        Button(onClick = {
+            coroutineScope.launch {
+                val result = snackBarHostState.showSnackbar(
+                    message = "This is a snackBar",
+                    actionLabel = "Undo",
+                    duration = SnackbarDuration.Short
+                )
+
+                when(result){
+                    SnackbarResult.Dismissed -> println("Dismissed")
+                    SnackbarResult.ActionPerformed -> println("Action performed")
+                }
+            }
+        }) {
+            Text(text = "Click Me")
         }
     }
 }
